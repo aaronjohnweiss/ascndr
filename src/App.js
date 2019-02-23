89cc8187f4ff2a2165ff04d678492841ce811186
Aaron Weiss <aaronjohnweiss@gmail.com>
2019-02-23 10:10:01 -0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="alert alert-primary" role="alert">
+          ASCNDr
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
