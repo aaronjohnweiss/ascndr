574236e056eb891104b6353c475ef70e15f18c85
Aaron Weiss <aaronjohnweiss@gmail.com>
2019-02-23 16:55:14 -0500

    Gym Modal

--- src/components/NewGymModal.js
+++ src/components/NewGymModal.js
@@ -1,30 +1,50 @@
 import React, { Component } from 'react'
 import Modal from 'react-bootstrap/es/Modal'
 import Button from 'react-bootstrap/es/Button'
+import Form from 'react-bootstrap/Form'
 
 export default class NewGymModal extends Component {
     constructor(props) {
         super(props)
 
-        this.state = {}
+        this.state = {
+            name: '',
+            location: '',
+            height: 0
+        }
+    }
+
+    onChange = (id) => (evt) => {
+        this.setState({ [id]: evt.target.value })
     }
 
     render() {
         const { handleClose, handleSubmit } = this.props
 
         return <Modal show={this.props.show} onHide={handleClose}>
-            <Modal.Header closeButton>
-                <Modal.Title>Add new gym</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>I'm an input form, I promise</Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                    Cancel
-                </Button>
-                <Button variant="primary" onClick={() => handleSubmit(this.state)}>
-                    Submit
-                </Button>
-            </Modal.Footer>
+            <Form>
+                <Modal.Header closeButton>
+                    <Modal.Title>
+                        Add New Gym
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form.Label>Gym Name</Form.Label>
+                    <Form.Control placeholder='Name..' onChange={this.onChange('name').bind(this)}/>
+                    <Form.Label>Gym Location</Form.Label>
+                    <Form.Control type='text' placeholder='Location..' onChange={this.onChange('location').bind(this)}/>
+                    <Form.Label>Average Wall Height (feet)</Form.Label>
+                    <Form.Control type='number' placeholder='Height..' onChange={this.onChange('height').bind(this)}/>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Cancel
+                    </Button>
+                    <Button variant="primary" onClick={() => handleSubmit(this.state)}>
+                        Submit
+                    </Button>
+                </Modal.Footer>
+            </Form>
         </Modal>
     }
 }
--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,7 @@
 const gyms = [
-    { id: 100, name: 'Gravity Vault' },
-    { id: 101, name: 'Rockville' },
-    { id: 102, name: 'Garden State Rocks' }
+    { id: 100, name: 'Gravity Vault', location: 'Flemington, NJ', height: 35 },
+    { id: 101, name: 'Rockville', location: 'Hamilton, NJ', height: 30 },
+    { id: 102, name: 'Garden State Rocks', location: 'Morganville, NJ', height: 28 }
 ]
 
 export default gyms
