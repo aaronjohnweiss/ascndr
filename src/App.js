38fba90aa08834d3df45bd7d939c6591bc3a7497
Aaron Weiss <aaronjohnweiss@gmail.com>
2019-02-23 20:25:54 -0500

    Pretty

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import GymIndex from './containers/GymIndex'
 import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom'
 import GymPage from './containers/GymPage'
+import { Jumbotron } from 'react-bootstrap'
+import './styles/styles.css'
 
 export default class App extends Component {
 
@@ -9,9 +11,10 @@ export default class App extends Component {
         return (
             <Router>
                 <div className="App">
-                    <div className='alert alert-primary text-center' role='alert'>
-                        ASCNDr
-                    </div>
+
+                    <Jumbotron>
+                        <h1 className='text-center'>ASCNDr</h1>
+                    </Jumbotron>
 
                     <Route exact path='/' render={() => <Redirect to='/gyms'/>}/>
                     <Route exact path='/gyms' component={GymIndex}/>
--- src/containers/GymPage.js
+++ src/containers/GymPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Col, Container, Row } from 'react-bootstrap'
 
 class GymPage extends Component {
 
@@ -16,19 +17,28 @@ class GymPage extends Component {
         const sessions = this.props.sessions.filter(session => session.gymId === id)
 
         return (
-            <div>
-                <h2>{gym.name}</h2>
-                <p>Location: {gym.location}</p>
-                <p>Height: {gym.height} ft</p>
-                <h3>Routes</h3>
-                {routes.map(route => (
-                    <p key={route.id}>{route.name}</p>
-                ))}
-                <h3>Sessions</h3>
-                {sessions.map(session => (
-                    <p key={session.id}>{new Date(session.startTime).toDateString()}</p>
-                ))}
-            </div>
+            <Container>
+                <Row>
+                    <Col md='2'></Col>
+                    <Col md='8'>
+
+                        <h2>{gym.name}</h2>
+                        <h4>
+                            <small>{gym.location}</small>
+                        </h4>
+                        <p>Average Wall Height: {gym.height} ft</p>
+                        <h3>Routes</h3>
+                        {routes.map(route => (
+                            <p key={route.id}>{route.name}</p>
+                        ))}
+                        <h3>Sessions</h3>
+                        {sessions.map(session => (
+                            <p key={session.id}>{new Date(session.startTime).toDateString()}</p>
+                        ))}
+                    </Col>
+                    <Col md='2'></Col>
+                </Row>
+            </Container>
 
         )
     }
