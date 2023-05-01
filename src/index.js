599cf475fb11892adaacd610c4ee507d679706e1
agilaz <erk700@gmail.com>
2023-04-30 23:43:19 -0400

    Update package versions

--- src/index.js
+++ src/index.js
@@ -2,22 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './App'
-import { Provider } from 'react-redux'
-import { applyMiddleware, createStore } from 'redux'
+import {Provider} from 'react-redux'
+import {applyMiddleware, createStore} from 'redux'
 import firebase from 'firebase/app'
-import { reactReduxFirebase } from 'react-redux-firebase'
+import {ReactReduxFirebaseProvider} from 'react-redux-firebase'
 import rootReducer from './redux/reducer'
 import thunkMiddleware from 'redux-thunk'
 import 'firebase/database'
 import * as serviceWorker from './serviceWorker'
 
-const createStoreWithFirebase = reactReduxFirebase(firebase, {})(createStore)
 
-const store = createStoreWithFirebase(rootReducer, undefined, applyMiddleware(thunkMiddleware))
+const rrfConfig = {
+    userProfile: 'users'
+}
+
+const store = createStore(rootReducer, undefined, applyMiddleware(thunkMiddleware))
+
+const rrfProps = {
+    firebase,
+    config: rrfConfig,
+    dispatch: store.dispatch
+}
 
 ReactDOM.render(
     <Provider store={store}>
-        <App/>
+        <ReactReduxFirebaseProvider {...rrfProps}>
+            <App/>
+        </ReactReduxFirebaseProvider>
     </Provider>,
     document.getElementById('root')
 )
