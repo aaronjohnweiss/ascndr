69e574e7719f617833d186035b92df7af1ae3793
agilaz <erk700@gmail.com>
2023-07-08 17:01:21 -0400

    Stateful feed data

--- src/containers/ActivityFeed.tsx
+++ src/containers/ActivityFeed.tsx
@@ -286,15 +286,31 @@ const ActivityFeed = () => {
 
     const [feedLength, setFeedLength] = useState(getNumberFromQuery(getQuery(location), 'n', DEFAULT_ITEMS))
 
+    const [feedData, setFeedData] = useState<FeedItem[]>([])
+
     useEffect(
         () => {
             history.replace({pathname: location.pathname, search: `?n=${feedLength}`})
         }, [feedLength]
     )
 
-    if (!isLoaded(gyms) || !isLoaded(sessions) || !isLoaded(users) || !isLoaded(routes) || !isLoaded(workouts)) return <></>
+    // Load the activity feed once all other data are loaded. Note this will not refresh in case of new content
+    useEffect(
+        () => {
+            if (isLoaded(gyms) && isLoaded(sessions) && isLoaded(users) && isLoaded(routes) && isLoaded(workouts) && feedData.length === 0) {
+                setFeedData(buildFeedData(uid, gyms, sessions, users, routes, workouts))
+            }
+        }, [
+            gyms,
+            sessions,
+            users,
+            routes,
+            workouts,
+            feedData
+        ]
+    )
 
-    const feedData = buildFeedData(uid, gyms, sessions, users, routes, workouts)
+    if (!isLoaded(gyms) || !isLoaded(sessions) || !isLoaded(users) || !isLoaded(routes) || !isLoaded(workouts)) return <></>
 
     return (
         <div className='activity-feed'>
