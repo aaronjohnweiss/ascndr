94a8e5092cb2041c834e25ea1395e747a00a5b1f
agilaz <erk700@gmail.com>
2023-07-08 18:34:52 -0400

    Comment date helpers

--- src/helpers/dateUtils.tsx
+++ src/helpers/dateUtils.tsx
@@ -9,10 +9,21 @@ export const dateString = (date: number | string | Date) => {
 
 export const localDateTimeString = (date: number | string | Date) => moment(date).format('YYYY-MM-DDTHH:mm:ss');
 
+/**
+ * Display relative time between the current time and the provided date (e.g. "2 weeks ago")
+ */
 export const timeFromNow = (date: number | string | Date) => moment(date).fromNow();
 
 
+/**
+ * Max time in the past for using a relative date instead of absolute date
+ */
 const MAX_RELATIVE_DATE = duration(1, 'month')
+
+/**
+ * Display relative time between the current date and the provided date (e.g. "2 weeks ago"),
+ * but once that gets too generic it switches to absolute dates (i.e. a date further in the past than {@link MAX_RELATIVE_DATE} will instead show like Jan 01 2000)
+ */
 export const preciseTimeFromNow = (date: number | string | Date) => {
     const age = duration(moment().diff(moment(date)))
 
